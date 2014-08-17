25b9ccb53644eae1a6f0350531dd9fbe1004b2a5
Rich Harris <richard.a.harris@gmail.com>
2014-08-17 17:52:42 -0400

    update gobblefile

--- gobblefile.js
+++ gobblefile.js
@@ -1,56 +1,36 @@
 var fs = require( 'fs' ),
 	path = require( 'path' ),
 	gobble = require( 'gobble' ),
-	esperanto;
 
-esperanto = (function () {
-	var methods = {
-		amd: 'toAmd',
-		cjs: 'toCjs'
-	};
+	dist, lib;
 
-	function esperanto ( es6, options ) {
-		var method = methods[ options.type || 'amd' ];
-
-		if ( !method ) {
-			throw new Error( 'The gobble-esperanto plugin supports the following types: amd, cjs' );
+// Compile a UMD version, via RequireJS and AMDClean
+dist = gobble( 'src' ).map( 'esperanto', { defaultOnly: true, addUseStrict: false })
+	.transform( 'requirejs', {
+		out: 'esperanto.js',
+		name: 'esperanto',
+		paths: {
+			acorn: 'empty:'
+		},
+		exclude: [ 'acorn' ],
+		optimize: 'none'
+	})
+	.map( 'amdclean', {
+		wrap: {
+			start: fs.readFileSync( 'wrapper/start.js' ).toString(),
+			end: fs.readFileSync( 'wrapper/end.js' ).toString()
 		}
-
-		return require( './lib/esperanto' )[ method ]( es6, options );
-	}
-
-	esperanto.defaults = {
-		accept: '.js'
-	};
-
-	return esperanto;
-}());
-
+	})
+	.map( 'jsbeautify', {
+		indent_with_tabs: true,
+		preserve_newlines: true
+	})
+	.moveTo( 'dist' );
 
 module.exports = [
-	// Compile a UMD version, via RequireJS and AMDClean
-	gobble( 'src' ).map( esperanto, { defaultOnly: true, addUseStrict: false })
-		.transform( 'requirejs', {
-			out: 'esperanto.js',
-			name: 'esperanto',
-			paths: {
-				acorn: 'empty:'
-			},
-			exclude: [ 'acorn' ],
-			optimize: 'none'
-		})
-		.map( 'amdclean', {
-			wrap: {
-				start: fs.readFileSync( 'wrapper/start.js' ).toString(),
-				end: fs.readFileSync( 'wrapper/end.js' ).toString()
-			}
-		})
-		.map( 'jsbeautify', {
-			indent_with_tabs: true,
-			preserve_newlines: true
-		})
-		.moveTo( 'dist' ),
+	dist,
+	dist.map( 'uglifyjs', { ext: '.min.js' }),
 
 	// Compile a node.js version
-	gobble( 'src' ).map( esperanto, { type: 'cjs', defaultOnly: true }).moveTo( 'lib' )
+	gobble( 'src' ).map( 'esperanto', { type: 'cjs', defaultOnly: true }).moveTo( 'lib' )
 ];
