33774abbdc11a7481cc35c32aec8f2204d169e8b
Rich Harris <richard.a.harris@gmail.com>
2014-08-16 21:31:32 -0400

    faster, cleaner output

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var recast = require( 'recast' ),
 	parse = require( './lib/parse' ),
+	transform = require( './lib/transform' ),
 	generators = require( './lib/generators' );
 
 module.exports = {
 	toAmd: function ( source, options ) {
 		options = options || {};
 
-		var parsed = parse( source, options );
-		return generators.amd( parsed, options );
+		var transformed = transform( source, options );
+		return generators.amd( transformed, options );
 	},
 
 	toCjs: function ( source, options ) {
 		options = options || {};
 
-		var parsed = parse( source, options );
-		return generators.cjs( parsed, options );
+		var transformed = transform( source, options );
+		return generators.cjs( transformed, options );
 	}
 };
