42e162d01d96b3be6e8cca2d4c2090ff36782a86
Rich Harris <richard.a.harris@gmail.com>
2014-08-17 21:35:16 -0400

    export var whatever should fail in defaultOnly mode

--- src/transform.js
+++ src/transform.js
@@ -131,6 +131,10 @@ export default function ( source, options, isAmd ) {
 
 			// Special case - `export var foo = 'bar'`
 			if ( node.declaration.type === 'VariableDeclaration' ) {
+				if ( options.defaultOnly ) {
+					throw new Error( 'A named export was used in defaultOnly mode' );
+				}
+
 				declaration = value + '\n' + indent;
 				value = node.declaration.declarations[0].id.name;
 			}
