71c91854eabdbee0cd83080ae2923126bc6acf87
Rich Harris <richard.a.harris@gmail.com>
2015-01-10 12:13:26 -0500

    add banner

--- gobblefile.js
+++ gobblefile.js
@@ -1,11 +1,17 @@
 var gobble = require( 'gobble' ),
-	node, browser;
+	banner, node, browser;
 
 gobble.cwd( __dirname );
 
+banner = require( 'fs' ).readFileSync( __dirname + '/src/banner.js', 'utf-8' )
+	.replace( '${VERSION}', require( './package.json' ).version )
+	.replace( '${TODAY}', today() );
+
 node = gobble( 'src' ).transform( 'esperanto-bundle', {
 	entry: 'esperanto',
-	type: 'cjs'
+	type: 'cjs',
+	banner: banner,
+	sourceMap: false
 });
 
 browser = gobble( 'src' ).transform( 'esperanto-bundle', {
@@ -13,9 +19,26 @@ browser = gobble( 'src' ).transform( 'esperanto-bundle', {
 	dest: 'esperanto.browser.js',
 	type: 'umd',
 	name: 'esperanto',
-	skip: [ 'bundler/getBundle' ]
+	skip: [ 'bundler/getBundle' ],
+	banner: banner,
+	sourceMap: false
 });
 
 module.exports = gobble([ node, browser ]).transform( 'es6-transpiler', {
 	disallowUnknownReferences: false
 });
+
+
+function today () {
+	var d = new Date();
+
+	return [
+		d.getFullYear(),
+		pad( d.getMonth() + 1 ),
+		pad( d.getDate() )
+	].join( '-' );
+}
+
+function pad ( num ) {
+	return num < 10 ? '0' + num : num;
+}
