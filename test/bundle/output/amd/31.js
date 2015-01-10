47e7450b3206e2e37e84e4e6715f8002e54ff54f
Rich Harris <richard.a.harris@gmail.com>
2015-01-10 15:43:15 -0500

    update tests

---
+++ test/bundle/output/amd/31.js
@@ -0,0 +1,11 @@
+define(function () {
+
+	'use strict';
+
+	function someOtherName () {}
+
+	function x () {}
+
+	someOtherName();
+
+});
---
+++ test/bundle/output/cjsDefaults/31.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function someOtherName () {}
+
+function x () {}
+
+someOtherName();
