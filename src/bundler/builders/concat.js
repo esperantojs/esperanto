e53b73fdf202f896b17e6c29f71ae999bc88b68e
Rich Harris <richard.a.harris@gmail.com>
2015-01-12 08:53:14 -0500

    support intro, outro, indent options with bundle.concat()

--- src/bundler/builders/concat.js
+++ src/bundler/builders/concat.js
@@ -1,18 +1,26 @@
 import packageResult from 'utils/packageResult';
 
 export default function concat ( bundle, options ) {
-	var body;
+	var body, intro, outro, indent;
 
 	// This bundle must be self-contained - no imports or exports
 	if ( bundle.externalModules.length || bundle.entryModule.exports.length ) {
 		throw new Error( 'bundle.concat() can only be used with bundles that have no imports/exports' );
 	}
 
-	body = bundle.body.clone()
-		.trimLines()
-		.indent()
-		.prepend( `(function () { 'use strict';\n\n` )
-		.append( '\n\n})();' );
+	body = bundle.body.clone();
+
+	// TODO test these options
+	intro = 'intro' in options ? options.intro : `(function () { 'use strict';\n\n`;
+	outro = 'outro' in options ? options.outro : '\n\n})();';
+
+	if ( !( 'indent' in options ) || options.indent === true ) {
+		indent = body.getIndentString();
+	} else {
+		indent = options.indent || '';
+	}
+
+	body.trimLines().indent( indent ).prepend( intro ).append( outro );
 
 	return packageResult( body, options, 'toString', true );
 }
