295647a4180583368ee44c7d5528e6d77fb06dda
Rich-Harris <richard.a.harris@gmail.com>
2015-03-26 19:17:13 -0400

    fix export { foo as bar } for one-to-one transformations (#127)

---
+++ test/samples/exportAs/_config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	description: 'export as',
+	strict: true
+};
---
+++ test/strictMode/output/amd/exportAs.js
@@ -0,0 +1,9 @@
+define(['exports'], function (exports) {
+
+	'use strict';
+
+	var foo = 42;
+
+	exports.bar = foo;
+
+});
