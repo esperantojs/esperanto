c6cf9478547b62f3056ca07df580712a2fd04be3
Rich-Harris <richard.a.harris@gmail.com>
2015-03-27 16:37:51 -0400

    optimise walker

--- src/utils/ast/walk.js
+++ src/utils/ast/walk.js
@@ -2,44 +2,49 @@ export default function walk ( ast, { enter, leave }) {
 	visit( ast, null, enter, leave );
 }
 
-function visit ( node, parent, enter, leave ) {
-	let shouldSkip = false;
+let context = {
+	skip: () => context.shouldSkip = true
+};
 
-	if ( enter ) {
-		enter.call({
-			skip: () => shouldSkip = true
-		}, node, parent );
-	}
-
-	if ( shouldSkip ) return;
-
-	let keys = Object.keys( node );
-	let key, value, i, j, numKeys, numChildren;
-
-	numKeys = keys.length;
-	for ( i = 0; i < numKeys; i += 1 ) {
-		key = keys[i];
-		value = node[ key ];
-
-		if ( isArray( value ) ) {
-			numChildren = value.length;
-			for ( j = 0; j < numChildren; j += 1 ) {
-				visit( value[j], node, enter, leave );
-			}
-		}
-
-		else if ( value && typeof value === 'object' ) {
-			visit( value, node, enter, leave );
-		}
-	}
-
-	if ( leave ) {
-		leave.call( null, node, parent );
-	}
-}
+let childKeys = {};
 
 let toString = Object.prototype.toString;
 
 function isArray ( thing ) {
 	return toString.call( thing ) === '[object Array]';
+}
+
+function visit ( node, parent, enter, leave ) {
+	if ( enter ) {
+		context.shouldSkip = false;
+		enter.call( context, node, parent );
+		if ( context.shouldSkip ) return;
+	}
+
+	let keys = childKeys[ node.type ] || (
+		childKeys[ node.type ] = Object.keys( node ).filter( key => typeof node[ key ] === 'object' )
+	);
+
+	let key, value, i, j;
+
+	i = keys.length;
+	while ( i-- ) {
+		key = keys[i];
+		value = node[ key ];
+
+		if ( isArray( value ) ) {
+			j = value.length;
+			while ( j-- ) {
+				visit( value[j], node, enter, leave );
+			}
+		}
+
+		else if ( value && value.type ) {
+			visit( value, node, enter, leave );
+		}
+	}
+
+	if ( leave ) {
+		leave( node, parent );
+	}
 }
