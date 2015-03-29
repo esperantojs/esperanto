69c4333353d09262cabacba344cdf93b663213d9
Rich-Harris <richard.a.harris@gmail.com>
2015-03-29 13:51:00 -0400

    simplify some things

--- src/bundler/combine/index.js
+++ src/bundler/combine/index.js
@@ -1,26 +1,18 @@
 import path from 'path';
 import MagicString from 'magic-string';
-import getUniqueNames from './getUniqueNames';
+import populateModuleNames from './populateModuleNames';
 import populateExternalModuleImports from './populateExternalModuleImports';
 import populateIdentifierReplacements from './populateIdentifierReplacements';
 import resolveExports from './resolveExports';
 import transformBody from './transformBody';
 
 export default function combine ( bundle ) {
-	let body = new MagicString.Bundle({
+	bundle.body = new MagicString.Bundle({
 		separator: '\n\n'
 	});
 
-	let declaredInBundle = bundle.modules.reduce( ( declared, mod ) => {
-		Object.keys( mod.ast._declared ).forEach( x => declared[x] = true );
-		return declared;
-	}, {} );
-
-	// populate names
-	let uniqueNames = getUniqueNames( bundle.modules, bundle.externalModules, bundle.names, declaredInBundle );
-	let setName = mod => mod.name = uniqueNames[ mod.id ];
-	bundle.modules.forEach( setName );
-	bundle.externalModules.forEach( setName );
+	// give each module in the bundle a unique name
+	populateModuleNames( bundle );
 
 	// determine which specifiers are imported from
 	// external modules
@@ -43,17 +35,15 @@ export default function combine ( bundle ) {
 
 			x.specifiers.forEach( s => {
 				if ( !importedModule.doesExport[ s.name ] ) {
-					throw new Error( 'Module ' + importedModule.id + ' does not export ' + s.name + ' (imported by ' + mod.id + ')' );
+					throw new Error( `Module '${importedModule.id}' does not export '${s.name}' (imported by '${mod.id}')` );
 				}
 			});
 		});
 
-		body.addSource({
+		bundle.body.addSource({
 			filename: path.resolve( bundle.base, mod.relativePath ),
 			content: transformBody( bundle, mod, mod.body ),
 			indentExclusionRanges: mod.ast._templateLiteralRanges
 		});
 	});
-
-	bundle.body = body;
 }
