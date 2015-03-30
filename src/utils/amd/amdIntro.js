5efb6bfa7a1fab9e999e5f2bac93b92c7f4dfc97
Rich Harris <richard.a.harris@gmail.com>
2015-03-30 11:29:59 -0400

    use amdIntro for standalone conversions

--- src/utils/amd/amdIntro.js
+++ src/utils/amd/amdIntro.js
@@ -1,8 +1,38 @@
-import { getId, getName, quote } from 'utils/mappers';
+import resolveId from '../resolveId';
+import { quote } from '../mappers';
 
-export default function amdIntro ({ name, imports, hasExports, indentStr }) {
-	let paths = imports.map( getId );
-	let names = imports.map( getName );
+export default function amdIntro ({ name, imports, hasExports, indentStr, absolutePaths }) {
+	let paths = [];
+	let names = [];
+	let seen = {};
+	let placeholders = 0;
+
+	imports.forEach( x => {
+		let path = x.id || x.path; // TODO unify these
+
+		if ( absolutePaths ) {
+			path = resolveId( path, name );
+		}
+
+		if ( !seen[ path ] ) {
+			seen[ path ] = true;
+
+			paths.push( path );
+
+			// TODO x could be an external module, or an internal one.
+			// they have different shapes, resulting in the confusing
+			// code below
+			if ( ( x.needsDefault || x.needsNamed ) || ( x.specifiers && x.specifiers.length ) ) {
+				while ( placeholders ) {
+					names.push( `__dep${names.length}__` );
+					placeholders--;
+				}
+				names.push( x.name );
+			} else {
+				placeholders++;
+			}
+		}
+	});
 
 	if ( hasExports ) {
 		paths.unshift( 'exports' );
