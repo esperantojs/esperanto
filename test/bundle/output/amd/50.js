c52a5e387ed6a3981d347402e63cbafbe7a62800
Rich Harris <richard.a.harris@gmail.com>
2015-04-17 16:01:07 -0400

    add expected results for #152 tests

---
+++ test/bundle/output/amd/50.js
@@ -0,0 +1,42 @@
+define(function () {
+
+	'use strict';
+
+	class A {
+		constructor () {
+			console.log( 'creating A' );
+		}
+
+		b () {
+			return new B();
+		}
+	}
+
+	class B extends A {
+		constructor () {
+			console.log( 'creating B' );
+		}
+	}
+
+	class D {
+		constructor () {
+			console.log( 'creating D' );
+		}
+
+		c () {
+			return new C();
+		}
+	}
+
+	class C extends D {
+		constructor () {
+			console.log( 'creating C' );
+		}
+	}
+
+	new A().b();
+	new B();
+	new C();
+	new D().c();
+
+});
---
+++ test/bundle/output/umd/50.js
@@ -0,0 +1,44 @@
+(function (factory) {
+	!(typeof exports === 'object' && typeof module !== 'undefined') &&
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory()
+}(function () { 'use strict';
+
+	class A {
+		constructor () {
+			console.log( 'creating A' );
+		}
+
+		b () {
+			return new B();
+		}
+	}
+
+	class B extends A {
+		constructor () {
+			console.log( 'creating B' );
+		}
+	}
+
+	class D {
+		constructor () {
+			console.log( 'creating D' );
+		}
+
+		c () {
+			return new C();
+		}
+	}
+
+	class C extends D {
+		constructor () {
+			console.log( 'creating C' );
+		}
+	}
+
+	new A().b();
+	new B();
+	new C();
+	new D().c();
+
+}));
