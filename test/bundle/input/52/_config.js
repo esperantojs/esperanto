7d0b487124c4f02eebaaf186d06c7cc583bf3b3a
Rich Harris <richard.a.harris@gmail.com>
2015-05-01 16:01:42 -0400

    fix test description

--- test/bundle/input/52/_config.js
+++ test/bundle/input/52/_config.js
@@ -1,7 +1,7 @@
 var path = require( 'path' );
 
 module.exports = {
-	description: 'allows imported bindings to share names with builtins',
+	description: 'correctly renames bindings from modules referred to by local and external-ish modules',
 	resolvePath: function ( importee ) {
 		return path.resolve( __dirname, 'external', importee.slice( 4 ) ) + '.js';
 	}
