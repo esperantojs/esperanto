29d19ab42d14e7dd63e8f4e14822392ed9b77c99
Rich-Harris <richard.a.harris@gmail.com>
2015-05-11 10:55:15 -0400

    use babel instead of es6-transpiler

--- gobblefile.js
+++ gobblefile.js
@@ -3,62 +3,64 @@ var path = require( 'path' );
 var resolve = require( 'resolve' );
 var Promise = require( 'sander' ).Promise;
 
-var banner, node, browser;
-
 gobble.cwd( __dirname );
 
-banner = require( 'fs' ).readFileSync( __dirname + '/src/banner.js', 'utf-8' )
+var banner = require( 'fs' ).readFileSync( __dirname + '/src/banner.js', 'utf-8' )
 	.replace( '${VERSION}', require( './package.json' ).version )
 	.replace( '${TODAY}', today() );
 
-node = gobble( 'src' ).transform( 'esperanto-bundle', {
-	entry: 'esperanto',
-	type: 'cjs',
-	banner: banner,
-	strict: true
-});
+var es5 = gobble( 'src' ).transform( 'babel' );
 
-browser = gobble( 'src' ).transform( 'esperanto-bundle', {
-	entry: 'esperanto',
-	dest: 'esperanto.browser.js',
-	type: 'umd',
-	name: 'esperanto',
-	skip: [ 'bundler/getBundle' ],
-	banner: banner,
-	strict: true,
+var node = es5
+	.transform( 'esperanto-bundle', {
+		entry: 'esperanto',
+		type: 'cjs',
+		banner: banner,
+		strict: true,
+		sourceMap: true
+	});
 
-	// bundle magic-string and its dependency, vlq
-	resolvePath: function ( importee, importer ) {
-		return new Promise( function ( fulfil, reject ) {
-			var callback = function ( err, result ) {
-				if ( err ) {
-					reject( err );
-				} else {
-					fulfil( result );
-				}
-			};
+var browser = es5
+	.transform( 'esperanto-bundle', {
+		entry: 'esperanto',
+		dest: 'esperanto.browser.js',
+		type: 'umd',
+		name: 'esperanto',
+		skip: [ 'bundler/getBundle' ],
+		banner: banner,
+		strict: true,
+		sourceMap: true,
 
-			resolve( importee, {
-				basedir: path.dirname( importer ),
-				packageFilter: function ( pkg ) {
-					if ( pkg[ 'jsnext:main' ] ) {
-						pkg.main = pkg[ 'jsnext:main' ];
-						return pkg;
+		// bundle magic-string and its dependency, vlq
+		resolvePath: function ( importee, importer ) {
+			return new Promise( function ( fulfil, reject ) {
+				var callback = function ( err, result ) {
+					if ( err ) {
+						reject( err );
+					} else {
+						fulfil( result );
 					}
+				};
 
-					var err = new Error( 'package ' + pkg.name + ' does not supply a jsnext:main field' );
-					err.code = 'ENOENT'; // hack
-					reject( err );
-					return {};
-				}
-			}, callback );
-		});
-	}
-});
+				resolve( importee, {
+					basedir: path.dirname( importer ),
+					packageFilter: function ( pkg ) {
+						if ( pkg[ 'jsnext:main' ] ) {
+							pkg.main = pkg[ 'jsnext:main' ];
+							return pkg;
+						}
 
-module.exports = gobble([ node, browser ]).transform( 'es6-transpiler', {
-	disallowUnknownReferences: false
-});
+						var err = new Error( 'package ' + pkg.name + ' does not supply a jsnext:main field' );
+						err.code = 'ENOENT'; // hack
+						reject( err );
+						return {};
+					}
+				}, callback );
+			});
+		}
+	});
+
+module.exports = gobble([ node, browser ]);
 
 
 function today () {
