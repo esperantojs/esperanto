901fbc643cbbe8c02c24cb122be15a3e1123be57
Rich-Harris <richard.a.harris@gmail.com>
2015-08-16 17:23:53 -0400

    prevent sourceMappingURL string being removed...

--- src/esperanto.js
+++ src/esperanto.js
@@ -165,7 +165,8 @@ export function bundle ( options ) {
 			});
 
 			if ( bundleOptions.sourceMap === 'inline' ) {
-				result.code += '\n//# sourceMappingURL=' + result.map.toUrl();
+				result.code += '\n//# sourceMap';
+				result.code += 'pingURL=' + result.map.toUrl();
 				result.map = null;
 			}
 
